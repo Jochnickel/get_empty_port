d66115ec7daf11bfc11cff80f98495123e5dd751
git <jj@jj22.de>
2020-03-17 20:55:55 +0000

    compiled alpine

--- getEmptyPort.c
+++ getEmptyPort.c
@@ -1,5 +1,5 @@
 #include <errno.h> // errno
-#include <unistd.h> // sleep
+#include <unistd.h> // sleep, close
 #include <strings.h> // bzero
 #include <error.h> // error
 #include <stdio.h> // puts printf
@@ -9,32 +9,32 @@
 
 int main() {
 
-        /* create a socket */
-        int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-        if (socket_desc<0)
-                error(/*exit code = */-1,/*stderr:*/ 9 /*== Bad file descriptor*/,"Could not create socket");
+ /* create a socket */
+ int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+ if (socket_desc<0)
+  error(/*exit code = */-1,/*stderr:*/ 9 /*== Bad file descriptor*/,"Could not create socket");
 
-        /* bind the socket */
-        struct sockaddr_in server;
-        server.sin_family = AF_INET;
-        server.sin_addr.s_addr = INADDR_ANY;
-        server.sin_port = htons( PORT );
+ /* bind the socket */
+ struct sockaddr_in server;
+ server.sin_family = AF_INET;
+ server.sin_addr.s_addr = INADDR_ANY;
+ server.sin_port = htons( PORT );
 
-        if ( bind(socket_desc,(struct sockaddr *)&server, sizeof(server)) < 0 )
-                error(-1,errno,"bind failed");
+ if ( bind(socket_desc,(struct sockaddr *)&server, sizeof(server)) < 0 )
+  error(-1,errno,"bind failed");
 
-        /* get the port number */
-        struct sockaddr_in my_addr;             bzero(&my_addr, sizeof(my_addr));
-        int len = sizeof(my_addr);
-        getsockname(socket_desc, (struct sockaddr *) &my_addr, &len);
+ /* get the port number */
+ struct sockaddr_in my_addr; bzero(&my_addr, sizeof(my_addr));
+ int len = sizeof(my_addr);
+ getsockname(socket_desc, (struct sockaddr *) &my_addr, &len);
 
-        unsigned int port = ntohs(my_addr.sin_port);
+ unsigned int port = ntohs(my_addr.sin_port);
 
-        listen(socket_desc , 3);
-        printf("Open port : %u\n", port); sleep(5);
+ listen(socket_desc , 3);
+ printf("Open port : %u\n", port);// sleep(5);
 
-        close(socket_desc);
-        puts("port closed"); sleep(10);
+ close(socket_desc);
+// puts("port closed"); sleep(10);
 
-        return port;
+ return port;
 }
